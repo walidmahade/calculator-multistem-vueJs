beed3d5ad2d37084437dcc6cdcf6b4c02495f7e2
Bd-Dev-007 <56132263+Bd-Dev-007@users.noreply.github.com>
2020-04-09 13:59:37 +0600

    added sales diff calculation ;

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ console.log("Start code");
 let headerBar = new Vue({
     el: '.header',
     data: {
-        progress: 20
+        progress: 25
     },
     methods: {
         resetForm: function () {
@@ -20,16 +20,17 @@ let calculator = new Vue({
     el: '#calculator',
     data: {
         totalScreens: 0,
-        currentActiveScreen: 2,
+        currentActiveScreen: 1,
+        formErrors: [],
         // calculation data
         availableMonths: [
-            'march', 'april', 'may'
+            'Mars', 'April', 'Mai'
         ],
         formData: {
-            selectMonth: 'april',
+            selectMonth: 'April',
             regularMonthlyIncome: '',
             afterCoronaMonthlyIncome: '',
-            salesDiff: '',
+            salesDiff: false,
         }
     },
     methods: {
@@ -52,13 +53,19 @@ let calculator = new Vue({
             this.currentActiveScreen = 1;
             this.regularMonthlyIncome = '';
             this.salesDiff = '';
+            this.formErrors = [];
             this.updateProgressBar(this.currentActiveScreen);
         },
         updateProgressBar(screen) {
-            let barProgress = screen * 20;
+            let barProgress = screen * 25;
             headerBar.progress = barProgress;
             headerBar.$refs.progressBar.style.setProperty('--progress-bar-width', barProgress + "%");
-        }
+        },
+        // calculateSalesDiff() {
+        //     // console.log('fired');
+        //     let salesDiff = (1 - this.formData.afterCoronaMonthlyIncome / this.formData.regularMonthlyIncome) * 100;
+        //     this.formData.salesDiff = salesDiff.toFixed(2);
+        // }
         // incrementRegularMonthlyIncome() {
         //     this.regularMonthlyIncome = this.regularMonthlyIncome ? this.regularMonthlyIncome + 50000 :  50000
         // },
@@ -67,7 +74,25 @@ let calculator = new Vue({
         // }
     },
     computed: {
-
+        salesDiff: function () {
+            this.formErrors = [];
+            if (this.formData.afterCoronaMonthlyIncome && this.formData.regularMonthlyIncome) {
+                let sd = (1 - this.formData.afterCoronaMonthlyIncome / this.formData.regularMonthlyIncome) * 100;
+                if (sd < 0) {
+                    this.formErrors.push("Salgsforskjellen må være positiv");
+                    this.formData.salesDiff = false;
+                } else if (sd > 0 && sd < 20) {
+                    this.formErrors.push("Forskjellen i salg må være 20% eller mer");
+                    this.formData.salesDiff = false;
+                } else {
+                    this.formData.salesDiff = true
+                }
+                return sd.toFixed(2);
+            } else {
+                this.formData.salesDiff = false;
+                return '';
+            }
+        }
     },
     created: function () {
         this.totalScreens = document.querySelectorAll('.screen').length;
