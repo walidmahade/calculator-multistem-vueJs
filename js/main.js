e44fe8d97fe4864b0406ebf9ee68e03e269699e8
Bd-Dev-007 <56132263+Bd-Dev-007@users.noreply.github.com>
2020-04-09 16:13:53 +0600

    added rest of the form logic

--- js/main.js
+++ js/main.js
@@ -30,11 +30,19 @@ let calculator = new Vue({
             selectMonth: 'April',
             regularMonthlyIncome: '',
             afterCoronaMonthlyIncome: '',
-            salesDiff: false,
+            salesDiffIsPositive: false,
+            salesDiff: '',
+            operationExpense: '',
+            govAid: {
+                closedByGov: '',
+                aidPercent: 0
+            }
+
         }
     },
     methods: {
         goNext: function () {
+            this.formErrors = [];
             // console.log(e.target)
             // let self = this;
             let testesPassed = this.currentActiveScreen < this.totalScreens;
@@ -49,29 +57,28 @@ let calculator = new Vue({
             this.currentActiveScreen = this.currentActiveScreen - 1;
             this.updateProgressBar(this.currentActiveScreen);
         },
-        resetForm() {
+        resetForm: function () {
             this.currentActiveScreen = 1;
             this.regularMonthlyIncome = '';
             this.salesDiff = '';
             this.formErrors = [];
             this.updateProgressBar(this.currentActiveScreen);
         },
-        updateProgressBar(screen) {
+        updateProgressBar: function(screen) {
             let barProgress = screen * 25;
             headerBar.progress = barProgress;
             headerBar.$refs.progressBar.style.setProperty('--progress-bar-width', barProgress + "%");
         },
-        // calculateSalesDiff() {
-        //     // console.log('fired');
-        //     let salesDiff = (1 - this.formData.afterCoronaMonthlyIncome / this.formData.regularMonthlyIncome) * 100;
-        //     this.formData.salesDiff = salesDiff.toFixed(2);
-        // }
-        // incrementRegularMonthlyIncome() {
-        //     this.regularMonthlyIncome = this.regularMonthlyIncome ? this.regularMonthlyIncome + 50000 :  50000
-        // },
-        // decrementRegularMonthlyIncome() {
-        //     this.regularMonthlyIncome = this.regularMonthlyIncome > 50000 ? this.regularMonthlyIncome - 50000 :  0
-        // }
+        wasClosedByGov: function (test) {
+            if (test) {
+                this.formData.govAid.aidPercent = .9;
+                this.formData.govAid.closedByGov = test // boolean
+            } else {
+                this.formData.govAid.aidPercent = .8;
+                this.formData.govAid.closedByGov = test // boolean
+            }
+            this.goNext();
+        }
     },
     computed: {
         salesDiff: function () {
@@ -80,16 +87,35 @@ let calculator = new Vue({
                 let sd = (1 - this.formData.afterCoronaMonthlyIncome / this.formData.regularMonthlyIncome) * 100;
                 if (sd < 0) {
                     this.formErrors.push("Salgsforskjellen må være positiv");
-                    this.formData.salesDiff = false;
+                    this.formData.salesDiffIsPositive = false;
                 } else if (sd > 0 && sd < 20) {
                     this.formErrors.push("Forskjellen i salg må være 20% eller mer");
-                    this.formData.salesDiff = false;
+                    this.formData.salesDiffIsPositive = false;
                 } else {
-                    this.formData.salesDiff = true
+                    this.formData.salesDiffIsPositive = true;
                 }
+                this.formData.salesDiff = sd.toFixed(2);
                 return sd.toFixed(2);
             } else {
-                this.formData.salesDiff = false;
+                this.formData.salesDiffIsPositive = false;
+                this.formData.salesDiff = '';
+                return '';
+            }
+        },
+        canApplyFor: function () {
+            let val;
+
+            if (this.formData.govAid.closedByGov) {
+                val = (this.formData.afterCoronaMonthlyIncome - this.formData.operationExpense) * (this.formData.salesDiff / 100) * this.formData.govAid.aidPercent;
+            } else {
+                val = (
+                    (((this.formData.salesDiff / 100) * this.formData.operationExpense) - 10000) * this.formData.govAid.aidPercent
+                )
+            }
+
+            if (val > 0 && this.formData.operationExpense) {
+                return val;
+            } else {
                 return '';
             }
         }
