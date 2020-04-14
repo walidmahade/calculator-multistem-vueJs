8549c424efa6d912ab142b3a20fca03e8200a22c
Bd-Dev-007 <56132263+Bd-Dev-007@users.noreply.github.com>
2020-04-15 00:24:13 +0600

    color text update

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ let headerBar = new Vue({
 
 // --------- vue instance for calculator
 let calculator = new Vue({
-    el: '#calculator',
+    el: '#calculator-wrap',
     data: {
         totalScreens: 0,
         currentActiveScreen: 1,
@@ -71,7 +71,7 @@ let calculator = new Vue({
             this.formErrors = [];
             this.updateProgressBar(this.currentActiveScreen);
         },
-        updateProgressBar: function(screen) {
+        updateProgressBar: function (screen) {
             let barProgress = screen * 25;
             headerBar.progress = barProgress;
             headerBar.$refs.progressBar.style.setProperty('--progress-bar-width', barProgress + "%");
@@ -96,8 +96,8 @@ let calculator = new Vue({
                     this.formErrors.push("Salgsforskjellen må være positiv");
                     this.formData.salesDiffIsPositive = false;
                 } else if (sd > 0 && sd < 20 && this.formData.selectMonth === 'Mars') {
-                    console.log(sd<20, "----", sd);
-                    this.formErrors.push("Nedgangen i omsettning må være 20% eller mer ssss");
+                    console.log(sd < 20, "----", sd);
+                    this.formErrors.push("Nedgangen i omsettning må være 20% eller mer");
                     this.formData.salesDiffIsPositive = false;
                 } else if (sd > 0 && sd < 30 && this.formData.selectMonth !== 'Mars') {
                     this.formErrors.push("Nedgangen i omsettning må være 30% eller mer");
